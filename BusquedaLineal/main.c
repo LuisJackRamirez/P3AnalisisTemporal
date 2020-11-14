777c7f46486b9939c9c8cd6510de4b74da3b22a4
luisrdevs <luisthread@gmail.com>
2020-11-14 11:36:24 -0600

    Codigo de busqueda lineal completo

---
+++ BusquedaLineal/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+
+int busquedaLineal(int n, int *A, int x);
+
+int main() {
+    int n, i, x;
+    scanf("%d %d", &n, &x);
+    int *A = malloc(sizeof(int) * n);
+    for(i = 0; i < n; ++i)
+        scanf("%d", &A[i]);
+    busquedaLineal(n, A, x) ?
+        printf("YES\n") : printf("NO\n");
+    return  0;
+}
+
+int busquedaLineal(int n, int *A, int x) {
+    int i;
+    for(i = 0; i < n; ++i)
+        if(A[i] == x)
+            return TRUE;
+    return FALSE;
+}
